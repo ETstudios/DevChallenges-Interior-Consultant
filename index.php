b49b000204ad59ba8910f99065640ead73e08abf
Eric Thomas <edt514@gmail.com>
2021-05-09 22:55:28 -0400

    Add challenge content

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+   
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Eric Thomas">
+	<meta name="description" content="Eric Thomas recreation of the DevChallenges Interior Consultant challenge">
+	<title> Interior Consultants </title>
+	<link rel="shortcut icon" href="img/icon.jpg">    
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/interior.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+   a
+</body>
+</html>
